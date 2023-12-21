1bbf36a958aa08b624c6fbba090df4384683fbae
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-21 22:54:28 +0530

    new changes

--- shipping.sh
+++ shipping.sh
@@ -25,3 +25,38 @@ then
 else
    echo "You are a root user"
 fi
+
+dnf install maven -y 
+VALIDATE $? " Installing maven "
+
+if [ $? -ne 0 ]
+then
+   useradd roboshop
+   VALIDATE $? "ROBOSHOP USER CREATION"
+else
+   echo -e " roboshop user already exits $Y SKIPPING $N "
+fi      
+
+mkdir -p /app 
+VALIDATE $? " creating app directory " 
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+cd /app
+unzip -o /tmp/shipping.zip
+
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+VALIDATE $? " reloading the daemon "
+systemctl enable shipping 
+VALIDATE $? " enabling the shipping "
+systemctl start shipping
+VALIDATE $? " starting shipping "
+
+dnf install mysql -y
+VALIDATE $? " installing my sql "
+
+mysql -h mysql.allmydevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+systemctl restart shipping
