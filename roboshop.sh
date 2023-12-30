66d92314f87c9bbc8b983622b9f558afc1cf3a3d
siddanipannaga <siddanipannaga24@gmail.com>
2023-12-30 22:39:21 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-03265a0778a880afb
 SG_ID=sg-0d25d5952a5f3c2e7 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z01756442QKET14NG09F4 # replace your zone ID
-DOMAIN_NAME= allmydevops.online
+DOMAIN_NAME=allmydevops.online
 
 for i in "${INSTANCES[@]}"
 do
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d25d5952a5f3c2e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
